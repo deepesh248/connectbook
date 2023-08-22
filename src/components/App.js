0c5e825e7fc00ce23d65e6f973e5d6f139821bc3
Deepesh <vermadeepesh248@gmail.com>
2023-08-22 12:10:40 +0530

    project setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique eum
+      dicta incidunt maxime consequatur tenetur nihil animi repudiandae
+      reprehenderit rem iusto quas non accusamus obcaecati illo sit, cum nam
+      eos! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+      voluptatem, totam magnam rerum similique, eius eveniet neque iure
+      exercitationem explicabo ullam deleniti ipsam assumenda ducimus harum
+      nobis quaerat accusantium vel?
+    </div>
+  );
+}
+
+export default App;
